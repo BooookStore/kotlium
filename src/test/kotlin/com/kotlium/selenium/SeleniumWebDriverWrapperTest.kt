8aed367f28be06dba228cd9d3a341a71dd34a02c
bookstore <booookstore@icloud.com>
2019-12-27 11:40:29 +0900

    test: SeleniumWebDriverWrapperTestを作成

---
+++ src/test/kotlin/com/kotlium/selenium/SeleniumWebDriverWrapperTest.kt
@@ -0,0 +1,40 @@
+package com.kotlium.selenium
+
+import com.kotlium.CssClass
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.RemoteWebDriver
+import java.net.URL
+
+class SeleniumWebDriverWrapperTest {
+
+    private lateinit var seleniumWebDriverWrapper: SeleniumWebDriverWrapper
+
+    private lateinit var driver: WebDriver
+
+    @BeforeEach
+    fun beforeEach() {
+        driver = RemoteWebDriver(URL("http://localhost:4444/wd/hub"), ChromeOptions())
+        driver.get("https://selenium.dev/")
+        seleniumWebDriverWrapper = SeleniumWebDriverWrapper(driver)
+    }
+
+    @AfterEach
+    fun afterEach() {
+        driver.close()
+    }
+
+    @Test
+    fun clickTest() {
+        // execute
+        seleniumWebDriverWrapper.click(CssClass("download-button webdriver"))
+
+        // verify
+        assertThat(driver.currentUrl).isEqualTo("https://selenium.dev/downloads/")
+    }
+
+}
