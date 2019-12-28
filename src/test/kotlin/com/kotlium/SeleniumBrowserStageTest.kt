c501ba488ef8dc8b5fc8f4adad84226b73ea6dca
bookstore <booookstore@icloud.com>
2019-12-28 20:59:17 +0900

    remove: テストコードで不要なコードを削除

--- src/test/kotlin/com/kotlium/SeleniumBrowserStageTest.kt
+++ src/test/kotlin/com/kotlium/SeleniumBrowserStageTest.kt
@@ -51,7 +51,6 @@ class SeleniumBrowserStageTest {
             }
         }.execute()
 
-        stageExecuteResult.executedActions.forEach { println(it.message.firstOrNull() ?: "") }
         assertThat(stageExecuteResult.isOk).isTrue()
     }
 
