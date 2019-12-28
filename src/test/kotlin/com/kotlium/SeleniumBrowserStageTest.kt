1b6b94d09269f57020c3de04d85d5fb0b7969454
bookstore <booookstore@icloud.com>
2019-12-28 21:11:47 +0900

    test: ステージの途中で処理に失敗した場合、ブラウザセッションが破棄されているかをテスト

--- src/test/kotlin/com/kotlium/SeleniumBrowserStageTest.kt
+++ src/test/kotlin/com/kotlium/SeleniumBrowserStageTest.kt
@@ -2,10 +2,12 @@ package com.kotlium
 
 import com.kotlium.selenium.SeleniumWebDriverWrapper
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By.xpath
+import org.openqa.selenium.NoSuchSessionException
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
@@ -27,7 +29,8 @@ class SeleniumBrowserStageTest {
     fun afterEach() {
         try {
             driver.close()
-        } catch (e: Exception) {
+            throw Exception("セッションが破棄されていません")
+        } catch (e: NoSuchSessionException) {
             // テスト内でセッションを破棄できなかった際のリトライのため、例外を無視
         }
     }
@@ -52,6 +55,26 @@ class SeleniumBrowserStageTest {
         }.execute()
 
         assertThat(stageExecuteResult.isOk).isTrue()
+    }
+
+    @Test
+    fun githubFailedTest() {
+        val config = BrowserStageConfiguration("github", "https://github.co.jp/")
+
+        // execute
+        val stageExecutedResult = BrowserStage(config, webDriverWrapper) {
+            click { target = xpath("//a[normalize-space() = '機能']") }
+            waitForUrl("https://github.co.jp/features")
+            assertPage {
+                assertThat(findElement(xpath("//*[normalize-space() = 'not exist element']")).isDisplayed).isTrue()
+            }
+            click { target = xpath("//a[normalize-space() = 'GitHub Marketplaceにアクセスする']") }
+        }.execute()
+
+        // verify
+        assertThatThrownBy { driver.close() }.isExactlyInstanceOf(NoSuchSessionException::class.java)
+        assertThat(stageExecutedResult.isOk).isFalse()
+        assertThat(stageExecutedResult.executedActions).hasSize(3)
     }
 
 }
