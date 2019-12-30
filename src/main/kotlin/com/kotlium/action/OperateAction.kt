e844576003198fc1ea947dc88c4e7f88027394a4
bookstore <booookstore@icloud.com>
2019-12-30 14:05:20 +0900

    refactor: IWebDriverWrapperを使用しないように変更

--- src/main/kotlin/com/kotlium/action/OperateAction.kt
+++ src/main/kotlin/com/kotlium/action/OperateAction.kt
@@ -3,6 +3,7 @@ package com.kotlium.action
 import com.kotlium.IWebDriverWrapper
 import com.kotlium.action.ActionType.OPERATOR
 import org.openqa.selenium.By
+import org.openqa.selenium.Keys
 
 data class TransitionAction(var url: String? = null) : Action {
 
@@ -34,19 +35,28 @@ data class ClickAction(override var target: By?) : SingleTargetAction(target) {
 
 }
 
-data class InputAction(
-    override var target: By? = null,
-    var value: String? = null,
-    var lastEnter: Boolean = false
-) : SingleTargetAction(target) {
+data class InputAction(override var target: By? = null, var value: String? = null, var lastEnter: Boolean = false) :
+    SingleTargetAction(target) {
 
     override fun execute(iWebDriverWrapper: IWebDriverWrapper): ActionExecuteResult {
-        val isOk = iWebDriverWrapper.input(
-            by = checkNotNull(target) { "input target is null" },
-            value = checkNotNull(value) { "input value is null" },
-            lastEnter = lastEnter
-        )
-        return ActionExecuteResult(InputAction::class, OPERATOR, isOk, listOf())
+        return try {
+            val driver = iWebDriverWrapper.driver
+            val inputContent = checkNotNull(value) { "input value is null" } + if (lastEnter) Keys.ENTER else ""
+            driver.findElement(checkNotNull(target) { "input target is null" }).sendKeys(inputContent)
+            ActionExecuteResult(
+                this::class, OPERATOR, true, listOf(
+                    "input $value to $target",
+                    "enter is $lastEnter"
+                )
+            )
+        } catch (e: Exception) {
+            ActionExecuteResult(
+                this::class, OPERATOR, false, listOf(
+                    "input failed $value to $target",
+                    "enter is $lastEnter"
+                )
+            )
+        }
     }
 
 }
