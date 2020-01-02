c6394bea7a4bf16c2bd6f910f944c932b60672f9
bookstore <booookstore@icloud.com>
2020-01-02 18:22:04 +0900

    modify: SLF4Jによるログ出力を実装

--- src/main/kotlin/com/kotlium/action/AssertAction.kt
+++ src/main/kotlin/com/kotlium/action/AssertAction.kt
@@ -4,12 +4,16 @@ import com.kotlium.action.ActionType.ASSERT
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.support.ui.ExpectedCondition
 import org.openqa.selenium.support.ui.WebDriverWait
+import org.slf4j.LoggerFactory
 
 data class PageAssertAction(val assert: WebDriver.() -> Unit) : Action {
+
+    private val logger = LoggerFactory.getLogger(PageAssertAction::class.java)
 
     override fun execute(webDriver: WebDriver): ActionExecuteResult {
         try {
             webDriver.assert()
+            logger.info("assertion filled")
             return ActionExecuteResult(
                 actionClass = PageAssertAction::class,
                 type = ASSERT,
@@ -17,6 +21,7 @@ data class PageAssertAction(val assert: WebDriver.() -> Unit) : Action {
                 message = listOf("assert filled")
             )
         } catch (e: Exception) {
+            logger.error("assertion failed", e)
             return ActionExecuteResult(
                 actionClass = PageAssertAction::class,
                 type = ASSERT,
@@ -30,9 +35,12 @@ data class PageAssertAction(val assert: WebDriver.() -> Unit) : Action {
 
 data class WaitForAssertAction<T>(val condition: ExpectedCondition<T>) : Action {
 
+    private val logger = LoggerFactory.getLogger(WaitForAssertAction::class.java)
+
     override fun execute(webDriver: WebDriver): ActionExecuteResult {
         try {
             WebDriverWait(webDriver, 5).until(condition)
+            logger.info("expected wait condition filled")
             return ActionExecuteResult(
                 actionClass = WaitForAssertAction::class,
                 type = ASSERT,
@@ -40,6 +48,7 @@ data class WaitForAssertAction<T>(val condition: ExpectedCondition<T>) : Action 
                 message = listOf("expected wait condition filled")
             )
         } catch (e: Exception) {
+            logger.error("expected wait condition not filled", e)
             return ActionExecuteResult(
                 actionClass = WaitForAssertAction::class,
                 type = ASSERT,
