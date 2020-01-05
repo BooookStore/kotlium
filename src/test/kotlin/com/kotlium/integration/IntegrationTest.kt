9f68e205fba05fedadb594cd52197df099dabc40
bookstore <booookstore@icloud.com>
2020-01-05 21:22:32 +0900

    modify: テスト対象を独自に開発したウェブアプリケーションに変更

--- src/test/kotlin/com/kotlium/integration/IntegrationTest.kt
+++ src/test/kotlin/com/kotlium/integration/IntegrationTest.kt
@@ -2,48 +2,48 @@ package com.kotlium.integration
 
 import com.kotlium.Scenario
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Rule
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By.xpath
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.support.ui.ExpectedConditions.urlToBe
 import org.testcontainers.containers.BrowserWebDriverContainer
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 class IntegrationTest {
 
-    @Rule
-    private val browserWebDriverContainer = BrowserWebDriverContainer<Nothing>()
-        .apply {
+    lateinit var browserWebDriverContainer: BrowserWebDriverContainer<Nothing>
+
+    @BeforeEach
+    fun beforeEach() {
+        val network = Network.newNetwork()
+
+        browserWebDriverContainer = BrowserWebDriverContainer<Nothing>().apply {
+            withNetwork(network)
             withCapabilities(ChromeOptions())
-            start()
         }
+
+        val testWebbApp = GenericContainer<Nothing>("bookstore/kotlium-test-webapp:0.0.1").apply {
+            withNetwork(network)
+            withExposedPorts(8080)
+            waitingFor(Wait.forHttp("/book/list"))
+            withNetworkAliases("testWebApp")
+        }
+
+        testWebbApp.start()
+        browserWebDriverContainer.start()
+    }
 
     @Test
     fun githubTest() {
         // setup and execute
         val scenarioExecuteResult = Scenario {
-            browserStage("https://github.co.jp/") {
-                click {
-                    xpath("//a[normalize-space() = '機能']")
-                }
-                waitFor {
-                    urlToBe("https://github.co.jp/features")
-                }
+            browserStage("testWebApp:8080/book/list") {
                 assertPage {
-                    assertThat(findElement(xpath("//*[normalize-space() = '効率的な開発ワークフロー']")).isDisplayed).isTrue()
-                }
-                click {
-                    xpath("//a[normalize-space() = 'GitHub Marketplaceにアクセスする']")
-                }
-                waitFor {
-                    urlToBe("https://github.com/marketplace")
-                }
-                input {
-                    target = xpath("//input[@name='query']")
-                    value = "circle ci"
-                    lastEnter = true
+                    assertThat(findElement(xpath("//body/p")).text).isEqualTo("ようこそ")
                 }
             }
         }.execute(browserWebDriverContainer.webDriver)
