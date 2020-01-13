7d215dd14af00b79249c040c2934557e047c77a8
bookstore <booookstore@icloud.com>
2020-01-13 19:13:30 +0900

    modify: テスト内容を変更

--- src/test/kotlin/com/kotlium/integration/IntegrationDatabaseTest.kt
+++ src/test/kotlin/com/kotlium/integration/IntegrationDatabaseTest.kt
@@ -1,20 +1,21 @@
 package com.kotlium.integration
 
-import com.kotlium.DatabaseStage
 import org.junit.jupiter.api.Test
+import java.util.*
 
 internal class IntegrationDatabaseTest {
 
     @Test
     fun integrationTest() {
-        // setup
-        val jdbcDriver = "..."
+        val properties = Properties().apply {
+            setProperty("user", "root")
+            setProperty("password", "password")
+        }
 
-        // execute
-        val executeResult = DatabaseStage {
-            assertTable {
-            }
-        }.execute(jdbcDriver)
+        val driver = com.mysql.cj.jdbc.Driver()
+        driver.connect("jdbc:mysql://localhost:3306", properties).use {
+            println("Connection Success!!!")
+        }
     }
 
 }
