ba4a751a42314f9daf9d78107f88f90c2a184f8c
bookstore <booookstore@icloud.com>
2020-01-18 17:50:16 +0900

    refactor: DatabaseActionを実装

--- src/main/kotlin/com/kotlium/database/OneRowAssertion.kt
+++ src/main/kotlin/com/kotlium/database/OneRowAssertion.kt
@@ -1,10 +1,17 @@
 package com.kotlium.database
 
 import java.sql.ResultSet
+import java.sql.Statement
 
-class OneRowAssertion {
+class OneRowAssertion : DatabaseAction {
+
+    override fun execute(statement: Statement): DatabaseActionExecuteResult {
+        return DatabaseActionExecuteResult(true, listOf())
+    }
+
     fun expected(function: ResultSet.() -> Unit) {
     }
 
     lateinit var query: String
+
 }
