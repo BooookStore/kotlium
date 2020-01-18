ad7511b26c7976e395a89abf9eb4e0c855cb10ba
bookstore <booookstore@icloud.com>
2020-01-18 18:10:04 +0900

    feat: OneRowAssertionの実装

--- src/main/kotlin/com/kotlium/database/OneRowAssertion.kt
+++ src/main/kotlin/com/kotlium/database/OneRowAssertion.kt
@@ -5,13 +5,27 @@ import java.sql.Statement
 
 class OneRowAssertion : DatabaseAction {
 
-    override fun execute(statement: Statement): DatabaseActionExecuteResult {
-        return DatabaseActionExecuteResult(true, listOf())
-    }
-
-    fun expected(function: ResultSet.() -> Unit) {
-    }
-
     lateinit var query: String
+
+    lateinit var assertionBlock: ResultSet.() -> Unit
+
+    override fun execute(statement: Statement): DatabaseActionExecuteResult {
+        val resultSet = statement.executeQuery(query)
+
+        if (!resultSet.next()) {
+            return DatabaseActionExecuteResult(false, listOf("not one row. actual row count is ${resultSet.fetchSize}"))
+        }
+
+        return runCatching {
+            assertionBlock.invoke(resultSet)
+        }.fold(
+            onSuccess = { DatabaseActionExecuteResult(true, listOf()) },
+            onFailure = { DatabaseActionExecuteResult(false, listOf()) }
+        )
+    }
+
+    fun expected(block: ResultSet.() -> Unit) {
+        assertionBlock = block
+    }
 
 }
