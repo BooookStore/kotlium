92f94a90e2f5af4b83f70489db23a800b40feaf9
bookstore <booookstore@icloud.com>
2020-02-12 21:17:50 +0900

    test: 追加

--- src/test/kotlin/com/kotlium/ScenarioTest.kt
+++ src/test/kotlin/com/kotlium/ScenarioTest.kt
@@ -5,10 +5,12 @@ import com.kotlium.action.BrowserActionType.OPERATOR
 import com.kotlium.action.ClickBrowserAction
 import com.kotlium.action.SessionCloseBrowserAction
 import com.kotlium.action.TransitionBrowserAction
+import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
 
 internal class ScenarioTest {
 
@@ -43,6 +45,48 @@ internal class ScenarioTest {
                     //@formatter:off
                     BrowserActionExecuteResult(TransitionBrowserAction::class, OPERATOR, true, listOf("transition http://example2.com")),
                     BrowserActionExecuteResult(ClickBrowserAction::class, OPERATOR, true, listOf("click By.id: id-for-element")),
+                    BrowserActionExecuteResult(SessionCloseBrowserAction::class, OPERATOR, true, listOf("close session"))
+                    //@formatter:on
+                )
+            )
+        )
+    }
+
+    @Test
+    fun scenarioExecuteFailedTest() {
+        // setup
+        val mockDriver: WebDriver = mockk(relaxed = true)
+        every { mockDriver.findElement(By.id("failed")) } throws NoSuchElementException()
+
+        // execute
+        val scenarioExecuteResult = Scenario {
+            browserStage("http://example1.com") {
+                click { By.id("id-for-element") }
+            }
+            browserStage("http://example2.com") {
+                click { By.id("failed") }
+            }
+        }.execute(mockDriver)
+
+        // verify
+        assertThat(scenarioExecuteResult.isOk).isFalse()
+        assertThat(scenarioExecuteResult.executedStages).containsExactly(
+            BrowserStageExecuteResult(
+                "http://example1.com",
+                listOf(
+                    //@formatter:off
+                    BrowserActionExecuteResult(TransitionBrowserAction::class, OPERATOR, true, listOf("transition http://example1.com")),
+                    BrowserActionExecuteResult(ClickBrowserAction::class, OPERATOR, true, listOf("click By.id: id-for-element")),
+                    BrowserActionExecuteResult(SessionCloseBrowserAction::class, OPERATOR, true, listOf("close session"))
+                    //@formatter:on
+                )
+            ),
+            BrowserStageExecuteResult(
+                "http://example2.com",
+                listOf(
+                    //@formatter:off
+                    BrowserActionExecuteResult(TransitionBrowserAction::class, OPERATOR, true, listOf("transition http://example2.com")),
+                    BrowserActionExecuteResult(ClickBrowserAction::class, OPERATOR, false, listOf("click failed By.id: failed")),
                     BrowserActionExecuteResult(SessionCloseBrowserAction::class, OPERATOR, true, listOf("close session"))
                     //@formatter:on
                 )
